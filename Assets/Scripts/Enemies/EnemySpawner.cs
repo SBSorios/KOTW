c7959e2f703efdeda364dad82ee1599d35af4892
SergioUdave <82485702+SergioUdave@users.noreply.github.com>
2021-04-21 18:07:30 -0600

    Enemies and Basic AI
    -Added an Enemies prefab folder
         -Added a Wind Enemy prefab
         -Added a Spawn Zones prefab, which can be attached to the Titan
    
    -Added an Enemies script folder
        -Added a Wind Enemy script
        -Added an Enemy Spawner script

---
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    // Choose object to spawn in the game
+    public GameObject objectToSpawn;
+
+
+   // Time variables
+    public float minTime;
+    public float maxTime;
+    private float curTime;
+    private float spawnTime;
+
+
+    // Spawnpoints 
+    private Transform[] spawnPoints;
+
+
+    void Awake()
+    {
+
+        List<Transform> spawningPointsAsList = new List<Transform>();
+
+        // Each child is considered a spawn point
+        foreach (Transform child in transform)
+        {
+            spawningPointsAsList.Add(child);
+        }
+
+        // Populate spawnPoints
+        spawnPoints = spawningPointsAsList.ToArray();
+    }
+
+
+    
+    void Start()
+    {
+        SetRandomTime();
+        curTime = 0;
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        // Count up from current time
+        curTime += Time.deltaTime;
+
+
+        // Check if its the right time to spawn the object
+        if (curTime >= spawnTime)
+        {
+
+            SpawnObject();
+            SetRandomTime();
+            curTime = 0;
+        }
+
+    }
+
+
+    void SetRandomTime()
+    {
+        // Set a random time between the minimum time and maximum time to spawn an object
+        spawnTime = Random.Range(minTime, maxTime);
+    }
+
+
+    void SpawnObject()
+    {
+        // Reset current time
+        curTime = 0;
+
+        // Find a random index between zero and one less than the number of spawn points
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+
+        // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation
+        Instantiate(objectToSpawn, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+
+    }
+}
