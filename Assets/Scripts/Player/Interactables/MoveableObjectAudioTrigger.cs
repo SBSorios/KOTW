5ced8e5cb4f242a8a1e85d768eae3c833e94de0f
ethancossio <ethancossio@hotmail.com>
2021-04-25 20:26:40 -0500

    Finished Designing 1 Minute Build Level
    -Playtested and tweaked until it was certain that the current build is playable.
    -Slotted in Space for potential gate interaction.
    -Added some SFX for interactable objects.

--- Assets/Scripts/Player/Interactables/MoveableObjectAudioTrigger.cs
+++ Assets/Scripts/Player/Interactables/MoveableObjectAudioTrigger.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WindBrushPushAudioTrigger : MonoBehaviour
+public class MoveableObjectAudioTrigger : MonoBehaviour
 {
     public AudioClip soundbyte;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-     private void OnTriggerEnter2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Cursor")
         {
