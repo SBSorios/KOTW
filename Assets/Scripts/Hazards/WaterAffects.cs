b601afc9fc17ec608987905cd3531e2a8dc4a88f
kndubich <kndubich@gmail.com>
2021-05-08 08:40:54 -0400

    Tutorial Level Complete
    ADDED
    UI Torch count:
    -Added a simple counter that currently tells the player how many torches are in the scene. However, will change it to be how many they have lit vs. how many are in scene
    CloudPuffAnimation prefab:
    -Simple cloud animation for our Kingfisher bird NPC
    Tutorial conversations:
    -All interactions should have a tutorial conversation with our bird npc inside the tutorial scene
    NPCBehavior script:
    -A script that adds a magical vibe to our bird
    
    MODIFIED
    CameraFollow:
    -Added functionality so we dont have to have the titan on when testing scenes
    GameManager script:
    -Same as above
    UIManager script:
    -Added variables for torch UI
    ToggleAffect script:
    -Gives UIManager the needed numbers for Torch UI

--- Assets/Scripts/Hazards/WaterAffects.cs
+++ Assets/Scripts/Hazards/WaterAffects.cs
@@ -20,7 +20,7 @@ public class WaterAffects : MonoBehaviour
         WaterLogged();
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
         {
